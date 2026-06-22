d25f01f723230c1408df9ba4d267c35abf503ddd
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-06-22 12:43:18 +0530

    Cards

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 
-	card := newCard()
-	fmt.Println(card)
-}
+	cards := newDeck()
 
-func newCard() string {
-	return "Five of Diamonds"
+	cards.print()
 }
